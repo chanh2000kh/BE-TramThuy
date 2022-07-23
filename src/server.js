e5a7d9b9061622a4361a3137f1e22fc26be2af94
Nguyen Duc Chanh <nguyenducchanh10c2@gmail.com>
2022-07-23 20:57:57 +0700

    Chanh

--- src/server.js
+++ src/server.js
@@ -38,7 +38,7 @@ app.get("/healCheck", (req, res) => {
 });
 app.get("/*", (req, res) => res.send({ message: "cannot access route" }));
 
-const port = 5000;
-app.listen(port, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${port}`);
 });
